349ffb1ffc8c2d5620e1731d1a8f5ccc2ebef660
Gregkids <79739949+Gregkids@users.noreply.github.com>
2025-07-07 14:37:27 +0700

    dummy database file for the program

---
+++ device_202507071408.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.device (device_id,device_name,"location",status,updated_at) VALUES
+	 ('DMMY02','DummyDeviceTablet',NULL,false,'2006-05-03 00:00:00+07'),
+	 ('DMMY01','DummyDevicePhone','-7.025737380032872, 107.54261434181912',true,'2024-07-06 10:00:00+07');
